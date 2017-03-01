7b1ad01e6169b92c6c66003534791e26920a0e39
Martyn Bissett <martynbissett@yahoo.co.uk>
2017-03-01 20:33:02 +0000

    Tidy up

--- gulpfile.js
+++ gulpfile.js
@@ -4,17 +4,16 @@ var gulp = require('gulp'),
     concat = require('gulp-concat'),
     watch = require('gulp-watch');
 
-// task for compiling sass, and minifying
+// Task for compiling sass, and minifying. Run with 'gulp sass'
 gulp.task('sass', function () {
 
+    // include paths for sass compiler
     var sassPaths = [
-        // 'bower_components/normalize.scss/sass',
         'bower_components/foundation-sites/scss',
         'bower_components/motion-ui/src',
         'scss'
     ];
 
-    // complile frontend less
     return gulp.src('scss/app.scss')
         .pipe(sass({
             includePaths: sassPaths,
@@ -23,26 +22,31 @@ gulp.task('sass', function () {
         .pipe(gulp.dest('dist/css'));
 });
 
-// task for compiling scripts, and minifying
+// Task for compiling scripts, and minifying. Run with 'gulp scripts'
 gulp.task('scripts', function() {
 
-    // combine all files
-    return gulp.src([
-            'bower_components/jquery/dist/jquery.js',
-            'bower_components/what-input/dist/what-input.js',
-            'bower_components/foundation-sites/dist/js/foundation.js',
-            'js/app.js'
-        ])
+    // Will compile all scripts into a single script. Add files to the array,
+    // scripts will be compiled in that order.
+    var scripts = [
+        'bower_components/jquery/dist/jquery.js',
+        'bower_components/what-input/dist/what-input.js',
+        'bower_components/foundation-sites/dist/js/foundation.js',
+        'js/app.js'
+    ];
+
+    return gulp.src(scripts)
         .pipe(concat('app.js'))
         .pipe(uglify()) // minify
         .pipe(gulp.dest('dist/js'));
 });
 
+// Gulp default task, will compile styles and scripts. Run with 'gulp'
 gulp.task('default', function() {
     gulp.start('sass');
     gulp.start('scripts');
 });
 
+// Run with 'gulp watch'
 gulp.task('watch', function () {
     watch(['scss/**/*.scss', 'js/**/*.js'], function (event) {
         gulp.start('sass');
