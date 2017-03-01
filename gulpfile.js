0941b2f91f534a4192c0b340ea2dd81aa76b2e6e
Martyn Bissett <martynbissett@yahoo.co.uk>
2017-03-01 20:00:14 +0000

    Tidy ups

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,14 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
-    minifyCss = require('gulp-minify-css'),
     uglify = require('gulp-uglify'),
     concat = require('gulp-concat'),
-    coffee = require('gulp-coffee'),
-    // sourcemaps = require('gulp-sourcemaps'),
     watch = require('gulp-watch');
 
-// task for compiling less, combining css, and minifying
+// task for compiling sass, and minifying
 gulp.task('sass', function () {
 
     var sassPaths = [
-        'bower_components/normalize.scss/sass',
+        // 'bower_components/normalize.scss/sass',
         'bower_components/foundation-sites/scss',
         'bower_components/motion-ui/src',
         'scss'
@@ -19,24 +16,25 @@ gulp.task('sass', function () {
 
     // complile frontend less
     return gulp.src('scss/app.scss')
-        // .pipe(sourcemaps.init())  // Process the original sources
         .pipe(sass({
             includePaths: sassPaths,
             outputStyle: 'compressed' // if css compressed **file size**
         }))
-        // .pipe(sourcemaps.write()) // Add the map to modified source.
         .pipe(gulp.dest('dist/css'));
 });
 
+// task for compiling scripts, and minifying
 gulp.task('scripts', function() {
 
     // combine all files
     return gulp.src([
+            'bower_components/jquery/dist/jquery.js',
+            'bower_components/what-input/dist/what-input.js',
+            'bower_components/foundation-sites/dist/js/foundation.js',
             'js/app.js'
         ])
         .pipe(concat('app.js'))
-        // //only uglify if gulp is ran with '--type production'
-        // .pipe(gutil.env.type === 'production' ? uglify() : gutil.noop())
+        .pipe(uglify()) // minify
         .pipe(gulp.dest('dist/js'));
 });
 
@@ -46,7 +44,7 @@ gulp.task('default', function() {
 });
 
 gulp.task('watch', function () {
-    watch(['scss/**/*.less', 'js/**/*.js'], function (event) {
+    watch(['scss/**/*.scss', 'js/**/*.js'], function (event) {
         gulp.start('sass');
         gulp.start('scripts');
     });
