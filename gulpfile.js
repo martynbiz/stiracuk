e98cb96e83ff8b5d974f164bd72320b3fd151ba7
Martyn Bisset <martyn.bisset1@stir.ac.uk>
2017-03-02 15:21:02 +0000

    Split css and scss compilation

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,12 @@ var gulp = require('gulp'),
     sass = require('gulp-sass'),
     uglify = require('gulp-uglify'),
     concat = require('gulp-concat'),
+    concatCss = require('gulp-concat-css'),
+    cleanCSS = require('gulp-clean-css'),
     watch = require('gulp-watch');
 
 // Task for compiling sass, and minifying. Run with 'gulp sass'
+//
 gulp.task('sass', function () {
 
     // include paths for sass compiler
@@ -19,11 +22,33 @@ gulp.task('sass', function () {
             includePaths: sassPaths,
             outputStyle: 'compressed' // if css compressed **file size**
         }))
+        .pipe(gulp.dest('src/css'));
+});
+
+// Task for compiling styles. Run with 'gulp css'. as we cannot
+// guarantee all style libraries will provide sass files, this task will
+// allow us to bring in css files too.
+gulp.task('css', function() {
+
+    // do any sass compilation prior to concating css files.
+    gulp.start('sass');
+
+    // Will compile all styles into a single file. Add files to the array,
+    // styles will be compiled in that order.
+    var scripts = [
+
+        //...
+        'src/css/app.css' // compiled app.sass
+    ];
+
+    return gulp.src(scripts)
+        .pipe(concatCss('app.css'))
+        .pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe(gulp.dest('dist/css'));
 });
 
-// Task for compiling scripts, and minifying. Run with 'gulp scripts'
-gulp.task('scripts', function() {
+// Task for compiling scripts, and minifying. Run with 'gulp js'
+gulp.task('js', function() {
 
     // Will compile all scripts into a single script. Add files to the array,
     // scripts will be compiled in that order.
@@ -31,6 +56,7 @@ gulp.task('scripts', function() {
         'bower_components/jquery/dist/jquery.js',
         'bower_components/what-input/dist/what-input.js',
         'bower_components/foundation-sites/dist/js/foundation.js',
+        //...
         'src/js/app.js'
     ];
 
@@ -42,14 +68,14 @@ gulp.task('scripts', function() {
 
 // Gulp default task, will compile styles and scripts. Run with 'gulp'
 gulp.task('default', function() {
-    gulp.start('sass');
-    gulp.start('scripts');
+    gulp.start('css');
+    gulp.start('js');
 });
 
 // Run with 'gulp watch'
 gulp.task('watch', function () {
     watch(['src/scss/**/*.scss', 'src/js/**/*.js'], function (event) {
-        gulp.start('sass');
-        gulp.start('scripts');
+        gulp.start('css');
+        gulp.start('js');
     });
 });
