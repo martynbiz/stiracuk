8b04cf7abfc3e0fc920dff020f2fef93b808e642
Martyn Bisset <martyn.bisset1@stir.ac.uk>
2017-03-28 13:48:09 +0100

    Fix gulpfile 2 save issue

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,10 @@
-var gulp = require('gulp'),
-    // js
-    uglify = require('gulp-uglify'),
-    concat = require('gulp-concat'),
-    // css
-    sass = require('gulp-sass'),
-    concatCss = require('gulp-concat-css'),
-    cleanCSS = require('gulp-clean-css'),
-    watch = require('gulp-watch');
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var sass = require('gulp-sass');
+var concatCss = require('gulp-concat-css');
+var cleanCSS = require('gulp-clean-css');
+// var watch = require('gulp-watch');
 
 
 // Task for compiling sass, and minifying. Run with 'gulp sass'
@@ -32,8 +30,8 @@ gulp.task('sass', function () {
 // allow us to bring in css files too.
 gulp.task('css', function() {
 
-    // do any sass compilation prior to concating css files.
-    gulp.start('sass');
+    // // do any sass compilation prior to concating css files.
+    // gulp.start('sass');
 
     // Will compile all styles into a single file. Add files to the array,
     // styles will be compiled in that order.
@@ -71,16 +69,25 @@ gulp.task('js', function() {
         .pipe(gulp.dest('dist/js'));
 });
 
-// Gulp default task, will compile styles and scripts. Run with 'gulp'
-gulp.task('default', function() {
-    gulp.start('css');
-    gulp.start('js');
+gulp.task('default', ['sass'], function() {
+    gulp.watch(['src/scss/**/*.scss'], ['sass']);
+    gulp.watch(['src/css/**/*.css'], ['css']);
+    gulp.watch(['src/js/**/*.js'], ['js']);
 });
 
-// Run with 'gulp watch'
-gulp.task('watch', function () {
-    watch(['src/scss/**/*.scss', 'src/js/**/*.js'], function (event) {
-        gulp.start('css');
-        gulp.start('js');
-    });
-});
+// // Gulp default task, will compile styles and scripts. Run with 'gulp'
+// gulp.task('default', function() {
+//     gulp.start('sass');
+//     gulp.start('js');
+// });
+//
+// // Run with 'gulp watch'
+// gulp.task('watch', function () {
+//     // watch(['src/scss/**/*.scss', 'src/js/**/*.js'], function (event) {
+//     //     gulp.start('css');
+//     //     gulp.start('js');
+//     // });
+//
+//     gulp.watch(['src/scss/**/*.scss'], ['sass']);
+//     // gulp.watch(['src/scss/**/*.scss'], ['sass']);
+// });
